f8ac960d3035f6841b7c9bdfc530ea39c4741063
farukaygun <farukaygun1@outlook.com>
2022-08-15 15:33:00 +0300

    Fixed file structure.
    - remove method has been changed to clean.
    - getOrigin method was changed from void to btVector3 as it should return the position of the object.

--- src/gipBulletPhysics.cpp
+++ src/gipBulletPhysics.cpp
@@ -1,8 +1,9 @@
 /*
- * gipEmpty.cpp
+ *  gipBulletPhysics.cpp
  *
- *  Created on: 2 Aug 2021
- *      Author: kayra
+ *  Created on: 5 Aug 2022
+ *      Author: Faruk Ayg�n
+ *      		Emirhan Limon
  */
 
 #include "gipBulletPhysics.h"
@@ -16,3 +17,43 @@ gipBulletPhysics::~gipBulletPhysics() {
 
 void gipBulletPhysics::update() {
 }
+
+void gipBulletPhysics::initialize() {
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher(collisionConfiguration);
+	overlappingPairCache = new btDbvtBroadphase();
+	solver = new btSequentialImpulseConstraintSolver ;
+	dynamicsWorld = new btDiscreteDynamicsWorld (dispatcher, overlappingPairCache, solver, collisionConfiguration);
+}
+
+void gipBulletPhysics::setGravity(float gravityValue) {
+	dynamicsWorld->setGravity(btVector3 (0, gravityValue, 0));
+}
+
+int gipBulletPhysics::stepSimulation(btScalar timeStep, int maxSubSteps , btScalar fixedTimeStep) {
+	return dynamicsWorld->stepSimulation(timeStep, maxSubSteps, fixedTimeStep);
+}
+
+int gipBulletPhysics::getNumCollisionObjects() {
+	return dynamicsWorld->getNumCollisionObjects();
+}
+
+btCollisionObjectArray& gipBulletPhysics::getCollisionObjectArray() {
+	return dynamicsWorld->getCollisionObjectArray();
+}
+
+/**@brief Return the origin vector translation */
+btVector3& gipBulletPhysics::getOrigin(btTransform* trans) {
+	return trans->getOrigin();
+}
+
+// Delete initialized objects
+void gipBulletPhysics::clean() {
+	delete collisionConfiguration;
+	delete dispatcher;
+	delete overlappingPairCache;
+	delete solver;
+	delete dynamicsWorld;
+}
+
+
