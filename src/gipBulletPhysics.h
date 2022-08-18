a5fc1207ba9728855112572b86b247753159374f
farukaygun <farukaygun1@outlook.com>
2022-08-18 17:47:39 +0300

    2d box and 2d circle creation methods added.
    - The creation methods create an object which has collider, transform, and rigidbody based on the given image.
    - Added getBox2dObjectPosition and getCircle2dObjectPosition methods that convert Bullet3 position to Glist Engine position.

--- src/gipBulletPhysics.h
+++ src/gipBulletPhysics.h
@@ -2,7 +2,7 @@
  *  gipBulletPhysics.h
  *
  *  Created on: 5 Aug 2022
- *      Author: Faruk Ayg�n,
+ *      Author: Faruk Aygun,
  *      		Emirhan Limon
  */
 
@@ -23,16 +23,27 @@ public:
 	void update();
 
 	void initialize();
+	// Delete initialized objects
 	void clean();
-
 	void setGravity(float gravityValue);
+	void addRigidBody(btRigidBody* rb);
+	void create2dBoxObject(gImage img, float x, float y, float objMass);
+	void create2dCircleObject(gImage img, float x, float y, float objMass);
+
 	// The btScalar type abstracts floating point numbers, to easily switch between double and single floating point precision.
 	int  stepSimulation(btScalar timeStep, int maxSubSteps = 1, btScalar fixedTimeStep = btScalar(1.) / btScalar(60.));
 	int  getNumCollisionObjects();
 
+	// Return the origin vector translation
 	btVector3& getOrigin(btTransform* trans);
+	// Unlike getOrigin, these two methods arranges and returns positions according to the Glist Engine.
+	btVector3 getCircle2dObjectPosition(btTransform trans, float imgWidth, float imgHeight);
+	btVector3 getBox2dObjectPosition(btTransform trans, float imgWidth, float imgHeight);
 	btCollisionObjectArray& getCollisionObjectArray();
 
+	// keep track of the shapes, we release memory at exit.
+	// make sure to re-use collision shapes among rigid bodies whenever possible!
+	btAlignedObjectArray<btCollisionShape*> collisionShapes;
 
 private:
 	btDefaultCollisionConfiguration* collisionConfiguration;
