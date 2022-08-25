0bf017a5842bbcdc0e29ffa42b59494eefd7d34b
farukaygun <farukaygun1@outlook.com>
2022-08-25 10:53:21 +0300

    Naming convention changed flattype from camelCase.

--- src/gipBulletPhysics.h
+++ src/gipBulletPhysics.h
@@ -25,20 +25,20 @@ public:
 	void initialize();
 	// Delete initialized objects
 	void clean();
-	void setGravity(float gravityValue);
+	void setGravity(float gravityvalue);
 	void addRigidBody(btRigidBody* rb);
-	void create2dBoxObject(gImageGameObject* imgobject, float objMass);
-	void create2dCircleObject(gImageGameObject* imgobject, float objMass);
+	void create2dBoxObject(gImageGameObject* imgobject, float objmass);
+	void create2dCircleObject(gImageGameObject* imgobject, float objmass);
 
 	// The btScalar type abstracts floating point numbers, to easily switch between double and single floating point precision.
-	int  stepSimulation(btScalar timeStep, int maxSubSteps = 1, btScalar fixedTimeStep = btScalar(1.) / btScalar(60.));
+	int  stepSimulation(btScalar timestep, int maxsubsteps = 1, btScalar fixedtimestep = btScalar(1.) / btScalar(60.));
 	int  getNumCollisionObjects();
 
 	// Return the origin vector translation
-	glm::vec2 getOrigin2d(btTransform* trans);
+	glm::vec2 getOrigin2d(btTransform* transform);
 	// Unlike getOrigin, these two methods arranges and returns positions according to the Glist Engine.
-	glm::vec2 getCircle2dObjectPosition(btTransform trans, float imgWidth, float imgHeight);
-	glm::vec2 getBox2dObjectPosition(btTransform trans, float imgWidth, float imgHeight);
+	glm::vec2 getCircle2dObjectPosition(btTransform transform, float imgwidth, float imgheight);
+	glm::vec2 getBox2dObjectPosition(btTransform transform, float imgwidth, float imgheight);
 
 	btCollisionObjectArray& getCollisionObjectArray();
 
